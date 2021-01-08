9766fff21542761cfc3c22d501d03180ebf14b87
Matteo Paggiaro <matteo.paggiaro@gmail.com>
2021-01-08 11:53:14 +0100

    Added try

---
+++ trycui/cmake-build-debug/CMakeFiles/trycui.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trycui.dir/main.cpp.o"
+  "trycui"
+  "trycui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trycui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
